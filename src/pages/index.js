4f94eccfcfc7729073842a37a6dc3a76830d7394
Laura Pham <l.pham2311@gmail.com>
2023-05-31 21:54:06 +1000

    to do list app with basic add function

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,176 +1,114 @@
-import * as React from "react"
+import * as React from 'react';
+import { useState, useEffect } from 'react';
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/getting-started/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+const defaultTodos = [
+	{
+		id: 1,
+		title: 'Walk Lunar',
+		complete: false,
+	},
+	{
+		id: 2,
+		title: 'Eat Breakfast',
+		complete: false,
+	},
+	{
+		id: 3,
+		title: 'Learn Japanese',
+		complete: false,
+	},
+];
 
 const IndexPage = () => {
-  return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
-  )
-}
+	const [todos, setToDos] = useState([]);
+	const [totalCompleted, setTotalCompleted] = useState(0);
+	useEffect(() => {
+		// if no todos in local storage, set default to dos
+		setToDos(defaultTodos);
+		// else use local storage todos
+	}, []);
 
-export default IndexPage
+	console.log({ todos, totalCompleted });
 
-export const Head = () => <title>Home Page</title>
+	useEffect(() => {
+		const totalCompleted = todos.reduce(
+			(acc, curr) => (curr.complete ? acc + 1 : acc),
+			0
+		);
+		setTotalCompleted(totalCompleted);
+	}, [todos]);
+
+	const handleSubmitToDo = (e) => {
+		e.preventDefault();
+		if (e.keyCode !== 13 || !e.target.value) return;
+		const newToDo = {
+			id: todos.length + 1,
+			title: e.target.value,
+			complete: false,
+		};
+		setToDos([...todos, newToDo]);
+		e.target.value = '';
+	};
+
+	const handleCheckedTodo = (e) => {
+		e.preventDefault();
+		const updatedTodoIndex = todos.findIndex(
+			(todo) => todo.id === parseInt(e.target.id, 10)
+		);
+		const updatedTodo = todos.find(
+			(todo) => todo.id === parseInt(e.target.id, 10)
+		);
+		todos[updatedTodoIndex] = {
+			...updatedTodo,
+			complete: !updatedTodo.complete,
+		};
+		setToDos([...todos]);
+	};
+
+	return (
+		<main className='flex justify-center flex-col items-center gap-2 font-mono p-10 h-full'>
+			<div className='z-10'>
+				<h1 className='text-2xl font-bold mb-2'>Daily To Do List</h1>
+				<p className='text-sm text-center'>
+					{totalCompleted}/{todos.length} ✅
+				</p>
+			</div>
+			<div
+				id='todo-list'
+				className='bg-white rounded border p-6 shadow-lg gap-4 flex flex-col absolute h-[100vh] top-0 max-w-[80vw] min-w-[50vw]'
+			>
+				<div id='todos' className='mt-[80px] overflow-auto'>
+					{todos.map((todo, index) => {
+						return (
+							<div className='flex gap-4 p-4 px-10 border-b last:border-b-0'>
+								<input
+									type='checkbox'
+									id={todo.id}
+									name='todo'
+									value={todo.title}
+									className='peer checked:bg-blue-[#23a6d5]'
+									checked={todo.complete}
+									onChange={handleCheckedTodo}
+								/>
+								<label for={todo.id} className='peer-checked:line-through '>
+									{todo.title}
+								</label>
+							</div>
+						);
+					})}
+				</div>
+				<input
+					class='placeholder:text-slate-400 block bg-white w-full border rounded-md p-2 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm text-center'
+					placeholder='Go for a run'
+					type='text'
+					name='add-todo'
+					onKeyUp={handleSubmitToDo}
+				/>
+			</div>
+		</main>
+	);
+};
+
+export default IndexPage;
+
+export const Head = () => <title>Home Page</title>;
